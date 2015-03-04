59f72b156168df43920cd619ac8584990b9bf0f8
Aitor <Aitor@Aitor-MSI>
2015-03-04 23:40:26 +0100

    primerC

---
+++ PROGRAMACION 2ev/TresRaya/src/tresraya/FicheroMarcador.java
@@ -0,0 +1,28 @@
+package tresraya;
+
+/**
+ *
+ * @author Aitor
+ */
+import java.io.*;
+
+public class FicheroMarcador {
+    
+    public FicheroMarcador(){
+        
+    }
+    public void LeerFichero(){
+        try{
+            
+        }catch(Exception a){
+            
+        }
+    }
+    public void EscribirFichero(){
+        try{
+            
+        }catch(Exception b){
+            
+        }
+    }
+}
